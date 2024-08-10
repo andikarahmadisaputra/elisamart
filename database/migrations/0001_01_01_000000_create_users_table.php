fc13d8ea1977268a44bd21a1a05f4e83bd041fc0
Andika Rahmadi Saputra <andikars811@gmail.com>
2024-08-10 10:23:44 +0700

    add some field in user table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('nik')->unique();
+            $table->enum('gender', ['pria', 'wanita']);
+            $table->decimal('balance', total: 12, places: 2);
+            $table->integer('pin');
             $table->rememberToken();
             $table->timestamps();
         });
